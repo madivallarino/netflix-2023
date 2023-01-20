ab5dc640648db780b2bd59543c4b29c2514afb03
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-01-20 14:46:02 -0500

    ready for deployment

--- src/Banner.js
+++ src/Banner.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Banner.css"
+import axios from './axios'
+import requests from "./Requests"
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+    
+    useEffect(()=> {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+        }
+
+        fetchData();
+    }, [])
+
+    console.log(movie)
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n - 1 ) + '...' : string;
+    }
+
   return (
     <header className='banner' style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
         backgroundPosition: "center center",
     }}>
-        <img />
+        <div className='banner__contents'>
+            <h1 className='banner__title'>
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
+            <div className="banner__buttons">
+                <button className='banner__button'>Play</button>
+                <button className='banner__button'>My List</button>
+            </div>
+            <h1 className="banner__description">
+                {truncate(movie?.overview, 150)}
+            </h1>
+        </div>
+        <div className="banner--fadeBottom" />
     </header>
   )
 }
